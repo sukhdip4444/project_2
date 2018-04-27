1f4b5bfc0f4bd0ae937105c6c025fb1b730aafcd
Sukhdip <sukhdip4444@gmail.com>
2018-04-27 14:43:48 +0530

    Angular configured working

--- angular_project_demo/conFusion/src/app/app.module.ts
+++ angular_project_demo/conFusion/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
