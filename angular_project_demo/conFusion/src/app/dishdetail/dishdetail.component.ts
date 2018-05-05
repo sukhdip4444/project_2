ca2e05fcb9ed1199683d609c548db4a028e3ca00
Sukhdip <sukhdip4444@gmail.com>
2018-05-05 11:16:02 +0530

    data binding

--- angular_project_demo/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ angular_project_demo/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {Dish}from "../shared/dish";
 
 @Component({
@@ -7,38 +7,9 @@ import {Dish}from "../shared/dish";
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-  dishes:Dish[]=[
-    {
-      name:'Uthappizza',
-      image: '/assets/images/uthappizza.png',
-      category: 'mains',
-      label:'Hot',
-      price:'4.99',
-      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-   {
-      name:'Zucchipakoda',
-      image: '/assets/images/zucchipakoda.png',
-      category: 'appetizer',
-      label:'',
-      price:'1.99',
-      description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-   {
-      name:'Vadonut',
-      image: '/assets/images/vadonut.png',
-      category: 'appetizer',
-      label:'New',
-      price:'1.99',
-      description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-      {
-        name:'ElaiCheese Cake',
-        image: '/assets/images/elaicheesecake.png',
-        category: 'dessert',
-        label:'',
-        price:'2.99',
-        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
-  ];
-
-  selectedDish = this.dishes[0];
+  
+  @Input()
+  dish: Dish;
 
   constructor() { }
 
