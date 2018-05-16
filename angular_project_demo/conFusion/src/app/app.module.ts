e39b2c611ac5fc6c9e95f4cf2bf780cf05b80864
Sukhdip <sukhdip4444@gmail.com>
2018-05-16 17:44:21 +0530

    single page application part1

--- angular_project_demo/conFusion/src/app/app.module.ts
+++ angular_project_demo/conFusion/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,7 @@ import { DishService } from './services/dish.service';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
